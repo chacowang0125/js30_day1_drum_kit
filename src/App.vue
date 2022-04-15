85bb524102341a3647bc9b8eb2bdf3e0a391eb46
chacowang0125 <chacowang0125@gmail.com>
2022-04-15 09:59:21 +0800

    feat: main functions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,169 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="keys">
+      <div
+        class="button"
+        v-for="key in keyList"
+        :key="key.keyCode"
+        :class="[key.isPlaying ? 'playing' : '']"
+        @transitionend="removeTransition($event, key)"
+      >
+        <div class="button_keyName">{{ key.keyName }}</div>
+        <div class="button_sound">{{ key.sound }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import clap from "../src/assets/sounds/clap.wav";
+import hihat from "../src/assets/sounds/hihat.wav";
+import kick from "../src/assets/sounds/kick.wav";
+import boom from "../src/assets/sounds/boom.wav";
+import openhat from "../src/assets/sounds/openhat.wav";
+import ride from "../src/assets/sounds/ride.wav";
+import snare from "../src/assets/sounds/snare.wav";
+import tom from "../src/assets/sounds/tom.wav";
+import tink from "../src/assets/sounds/tink.wav";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      keyList: [
+        {
+          keyName: "A",
+          sound: "CLAP",
+          keyCode: 65,
+          soundSrc: clap,
+          isPlaying: false,
+        },
+        {
+          keyName: "S",
+          sound: "HIHAT",
+          keyCode: 83,
+          soundSrc: hihat,
+          isPlaying: false,
+        },
+        {
+          keyName: "D",
+          sound: "KICK",
+          keyCode: 68,
+          soundSrc: kick,
+          isPlaying: false,
+        },
+        {
+          keyName: "F",
+          sound: "OPENHAT",
+          keyCode: 70,
+          soundSrc: openhat,
+          isPlaying: false,
+        },
+        {
+          keyName: "G",
+          sound: "BOOM",
+          keyCode: 71,
+          soundSrc: boom,
+          isPlaying: false,
+        },
+        {
+          keyName: "H",
+          sound: "RIDE",
+          keyCode: 72,
+          soundSrc: ride,
+          isPlaying: false,
+        },
+        {
+          keyName: "J",
+          sound: "SNARE",
+          keyCode: 74,
+          soundSrc: snare,
+          isPlaying: false,
+        },
+        {
+          keyName: "K",
+          sound: "TOM",
+          keyCode: 75,
+          soundSrc: tom,
+          isPlaying: false,
+        },
+        {
+          keyName: "L",
+          sound: "TINK",
+          keyCode: 76,
+          soundSrc: tink,
+          isPlaying: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    playSound(e) {
+      const key = this.keyList.find((key) => key.keyCode === e.keyCode);
+      if (!key) {
+        return;
+      }
+      const audio = new Audio(key.soundSrc);
+      audio.play();
+      key.isPlaying = true;
+    },
+    removeTransition(e, key) {
+      if (e.propertyName !== "transform") return;
+      key.isPlaying = false;
+    },
+  },
+  created() {
+    window.addEventListener("keydown", this.playSound);
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  font-size: 10px;
+  background: url("./assets/background.jpg") bottom center;
+  background-size: cover;
+}
+
+body,
+html {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+.keys {
+  display: flex;
+  flex: 1;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+.button {
+  border: 0.4rem solid black;
+  border-radius: 0.5rem;
+  margin: 1rem;
+  font-size: 1.5rem;
+  padding: 1rem 0.5rem;
+  transition: all 0.07s ease;
+  width: 10rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background: rgba(0, 0, 0, 0.4);
+  text-shadow: 0 0 0.5rem black;
+  cursor: pointer;
+}
+.button_keyName {
+  display: block;
+  font-size: 4rem;
+}
+.button_sound {
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  color: #ffc600;
+}
+.playing {
+  transform: scale(1.1);
+  border-color: #ffc600;
+  box-shadow: 0 0 1rem #ffc600;
 }
 </style>
